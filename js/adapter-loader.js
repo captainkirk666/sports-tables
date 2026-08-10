ec9c4443d1281e73b83ffb906c027efa4a98de90
captainkirk666 <contactkirk@hotmail.com>
2026-08-10 19:13:17 +1000

    Update EPL adapter version from 2 to 3

--- js/adapter-loader.js
+++ js/adapter-loader.js
@@ -24,7 +24,7 @@
 (function () {
   var ADAPTER_VERSIONS = {
     f1: "7",
-    epl: "2"
+    epl: "3"
   };
   var BASE = "https://captainkirk666.github.io/sports-tables";
   var sport = document.currentScript.getAttribute('data-sport');
