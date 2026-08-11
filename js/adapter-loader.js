501b8f66c25373ec4c5854b4ed7d69cad04af7a7
captainkirk666 <contactkirk@hotmail.com>
2026-08-11 18:07:41 +1000

    Update adapter-loader.js

--- js/adapter-loader.js
+++ js/adapter-loader.js
@@ -24,7 +24,7 @@
 (function () {
   var ADAPTER_VERSIONS = {
     f1: "7",
-    epl: "7"
+    epl: "8"
   };
   var BASE = "https://captainkirk666.github.io/sports-tables";
   var sport = document.currentScript.getAttribute('data-sport');
